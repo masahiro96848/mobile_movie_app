c4ff150fd93f5a96540f257dcf5bab24aa6b4f08
masahiro96848 <feetwcpic9436@gmail.com>
2025-08-02 23:23:51 +0900

    tailwindのextendにデザインの拡張を追加

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl font-bold text-dark-200">Welcome!</Text>
     </View>
   )
 }
