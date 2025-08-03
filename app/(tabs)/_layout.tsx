07cf14a8d79bce3d9e7c7e9bf05e107f1a478bd2
masahiro96848 <feetwcpic9436@gmail.com>
2025-08-03 09:42:36 +0900

    タブアイコンのコンポーネントを作成

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,27 @@
+import { icons } from '@/constants/icons'
 import { Tabs } from 'expo-router'
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { images } from '../../constants/images'
+
+const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold">{title}</Text>
+      </ImageBackground>
+    )
+  }
+  return (
+    <View className="size-full justify-center items-center  mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  )
+}
 
 export default function _layout() {
   return (
@@ -10,6 +31,9 @@ export default function _layout() {
         options={{
           title: 'Home',
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
         }}
       />
       <Tabs.Screen
@@ -17,6 +41,9 @@ export default function _layout() {
         options={{
           title: 'Search',
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} title="Search" />
+          ),
         }}
       />
       <Tabs.Screen
@@ -24,6 +51,9 @@ export default function _layout() {
         options={{
           title: 'Saved',
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.save} title="Saved" />
+          ),
         }}
       />
       <Tabs.Screen
@@ -31,6 +61,9 @@ export default function _layout() {
         options={{
           title: 'Profile',
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.person} title="Profile" />
+          ),
         }}
       />
     </Tabs>
